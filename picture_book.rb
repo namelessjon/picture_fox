18f02c425fea1f57091e00a6587baa8dd6ef7ed4
Jonathan Stott <jonathan.stott@gmail.com>
2008-05-21 19:53:08 +0100

    PictureBook now displays a single photo
    Well, actually it doesn't, but it tries at least.  More changes need to be made
    to various subclasses to make it display properly.

--- picture_book.rb
+++ picture_book.rb
@@ -2,16 +2,22 @@
 # picture_book.rb
 # Jonathan D. Stott <jonathan.stott@gmail.com>
 # Created: Wednesday, May 21, 2008 @ 19:25
-# Modified: Wednesday, May 21, 2008 @ 19:27
-
+# Modified: Wednesday, May 21, 2008 @ 19:51
+$:.unshift File.join(File.dirname(__FILE__),"app/models")
+$:.unshift File.join(File.dirname(__FILE__),"app/views")
 require 'rubygems'
 require 'fox16'
 
 include Fox
 
+require 'photo'
+require 'photo_view'
+
 class PictureBook < FXMainWindow
   def initialize(app)
     super(app, "Picture Book", :width => 600, :height => 400)
+    photo = Photo.new("/home/jon/temp_img/6986933-lg.jpg")
+    photo_view = PhotoView.new(self, photo)
   end
 
   def create
