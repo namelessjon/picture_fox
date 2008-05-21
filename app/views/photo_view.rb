61db6d98102e99252bdcf4fff422e0a8b31d09d6
Jonathan Stott <jonathan.stott@gmail.com>
2008-05-21 19:41:22 +0100

    very basic photo_view

---
+++ app/views/photo_view.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+# photo_view.rb
+# Jonathan D. Stott <jonathan.stott@gmail.com>
+# Created: Wednesday, May 21, 2008 @ 19:29
+# Modified: Wednesday, May 21, 2008 @ 19:30
+
+class PhotoView < FXImageFrame
+  def initialize(p, photo)
+    super(p, nil)
+  end
+end
