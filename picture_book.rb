111e089373995229113ab84600b6c81947d54a93
Jonathan Stott <jonathan.stott@gmail.com>
2008-05-21 21:58:23 +0100

    Added menu bar and ability to import photos from a directory

--- picture_book.rb
+++ picture_book.rb
@@ -2,7 +2,7 @@
 # picture_book.rb
 # Jonathan D. Stott <jonathan.stott@gmail.com>
 # Created: Wednesday, May 21, 2008 @ 19:25
-# Modified: Wednesday, May 21, 2008 @ 20:10
+# Modified: Wednesday, May 21, 2008 @ 21:56
 $:.unshift File.join(File.dirname(__FILE__),"app/models")
 $:.unshift File.join(File.dirname(__FILE__),"app/views")
 require 'rubygems'
@@ -17,9 +17,8 @@ require 'album_view'
 class PictureBook < FXMainWindow
   def initialize(app)
     super(app, "Picture Book", :width => 600, :height => 400)
+    add_menu_bar
     @album = Album.new("My Album")
-    @album << Photo.new("/home/jon/temp_img/6986933-lg.jpg")
-    @album << Photo.new("/home/jon/temp_img/XQTdQMQu942b5w9qHVMVJhEX_500.jpg")
     @album_view = AlbumView.new(self, @album)
   end
 
@@ -27,6 +26,51 @@ class PictureBook < FXMainWindow
     super
     show(PLACEMENT_SCREEN)
   end
+
+
+  # adds a menu bar with a few options
+  def add_menu_bar
+    # menubar across the top, the width of the whole screen
+    menu_bar = FXMenuBar.new(self, LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
+
+    # will hold all the commands
+    file_menu = FXMenuPane.new(self)
+
+    # add a file menu to the menubar
+    FXMenuTitle.new(menu_bar, "File", :popupMenu => file_menu)
+
+    # now setup some actual menu commands
+    # First, the one which will display our select box
+    import_cmd = FXMenuCommand.new(file_menu, "Import ...")
+    import_cmd.connect(SEL_COMMAND) do
+      dialog = FXFileDialog.new(self, "Import Photos")
+      dialog.selectMode = SELECTFILE_MULTIPLE
+      dialog.patternList = ["JPEG Images (*.jpg, *.jpeg)"]
+      if dialog.execute != 0
+        import_photos(dialog.filenames)
+      end
+    end
+
+    # a seperator
+    FXMenuSeparator.new(file_menu)
+
+    # And an exit command
+    exit_cmd = FXMenuCommand.new(file_menu, "Exit")
+    exit_cmd.connect(SEL_COMMAND) do
+      exit
+    end
+  end
+
+  def import_photos(filenames)
+    filenames.each do |fn|
+      p = Photo.new(fn)
+      @album << p
+      @album_view.add_photo(p)
+    end
+    @album_view.create
+  end
+
+
 end
 
 if __FILE__ == $0
