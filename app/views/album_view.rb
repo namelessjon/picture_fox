0ccc7c54c2721a4df59af7d6895f787f39259209
Jonathan Stott <jonathan.stott@gmail.com>
2008-05-21 22:10:59 +0100

    The photolist is now scrollable.

--- app/views/album_view.rb
+++ app/views/album_view.rb
@@ -2,29 +2,34 @@
 # album_view.rb
 # Jonathan D. Stott <jonathan.stott@gmail.com>
 # Created: Wednesday, May 21, 2008 @ 20:03
-# Modified: Wednesday, May 21, 2008 @ 22:01
+# Modified: Wednesday, May 21, 2008 @ 22:09
 require 'photo_view'
 
-class AlbumView < FXMatrix
+class AlbumView < FXScrollWindow
 
   attr_reader :album
 
   def initialize(p, album)
     # LAYOUT_FILL is fairly ronseal!
-    super(p, :opts => LAYOUT_FILL|MATRIX_BY_COLUMNS)
+    super(p, :opts => LAYOUT_FILL)
     @album = album
-    @album.each {|p| add_photo(p) }
+    FXMatrix.new(self, :opts => LAYOUT_FILL|MATRIX_BY_COLUMNS)
+    @album.each {|photo| add_photo(photo) }
   end
 
+  
   def add_photo(photo)
-    PhotoView.new(self, photo)
+    # contentWindow is the content of the FXScrollWindow
+    # Thanks to the FXMatrix.new(self ...) declaration up there, it's that FXMatrix
+    # See FXRuby, pg 52
+    PhotoView.new(contentWindow, photo)
   end
 
   # layout gets called on each resize automagically by fxruby
   def layout
     # number of columns = the most we can fit on one row, or 1 if that is 
     # too small
-    self.numColumns = [width/PhotoView::MAX_WIDTH,1].max
+    contentWindow.numColumns = [width/PhotoView::MAX_WIDTH,1].max
     super
   end
 end
