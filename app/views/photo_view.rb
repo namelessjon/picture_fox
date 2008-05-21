7a21587b8edc535ae2cdc313e166686aafb3468b
Jonathan Stott <jonathan.stott@gmail.com>
2008-05-21 20:02:22 +0100

    PhotoView now loads a jpg image, at least

--- app/views/photo_view.rb
+++ app/views/photo_view.rb
@@ -2,10 +2,17 @@
 # photo_view.rb
 # Jonathan D. Stott <jonathan.stott@gmail.com>
 # Created: Wednesday, May 21, 2008 @ 19:29
-# Modified: Wednesday, May 21, 2008 @ 19:30
+# Modified: Wednesday, May 21, 2008 @ 19:56
 
 class PhotoView < FXImageFrame
   def initialize(p, photo)
     super(p, nil)
+    load_image(photo.path)
+  end
+
+  def load_image(path)
+    File.open(path, "rb") do |io|
+      self.image = FXJPGImage.new(app, io.read)
+    end
   end
 end
