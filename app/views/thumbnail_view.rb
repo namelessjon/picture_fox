fccb6b16c6b4531e1e15bafafd7cc333007faba4
Jonathan Stott <jonathan.stott@gmail.com>
2008-06-03 19:19:31 +0100

    Photo model is now an instance variable of the ThumbnailView

--- app/views/thumbnail_view.rb
+++ app/views/thumbnail_view.rb
@@ -1,10 +1,14 @@
 class ThumbnailView < FXImageFrame
+
+  attr_accessor :photo
+
   def initialize(p, photo)
     super(p, nil)
-    load_image(photo)
+    self.photo = photo
+    load_image
   end
 
-  def load_image(photo)
+  def load_image
     self.image = FXPNGImage.new(app, photo.thumbnail)
   end
 end
